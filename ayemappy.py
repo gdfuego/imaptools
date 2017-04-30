b19e2726cddc69d363c73e4a5780656ffff53c0b
Gregory Boyce <gregory.boyce@gmail.com>
2017-04-30 16:08:34 -0400

    Split library and tool

--- ayemappy.py
+++ ayemappy.py
@@ -54,48 +54,8 @@ class ayemappy:
         self.Session.close()
         self.Session.logout()
 
-def ensure_dir(file_path):
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
 def main():
-    import getpass
-
-    imapserver = raw_input('Enter the server name:')
-    userName = raw_input('Enter your username:')
-    passwd = getpass.getpass('Enter your password: ')
-
-    M = ayemappy(imapserver, userName, passwd)
-    for folder in M.listfolders():
-	print 'Processing %s' % folder
-
-	# Iterating over all emails
-        for msgId in tqdm(M.listmessages(folder)):
-	    typ, messageParts = M.Session.fetch(msgId, '(RFC822)')
-	    if typ != 'OK':
-		print 'Error fetching mail.'
-		raise
-
-	    emailBody = messageParts[0][1]
-	    mail = email.message_from_string(emailBody)
-	    for part in mail.walk():
-		if part.get_content_maintype() == 'multipart':
-		    continue
-		if part.get('Content-Disposition') is None:
-		    continue
-		fileName = part.get_filename()
-
-		if bool(fileName):
-		    filePath = os.path.join(".", 'downloads', M.server, M.user, fileName)
-                    ensure_dir(filePath)
-		    if not os.path.isfile(filePath) :
-			print fileName
-			fp = open(filePath, 'wb')
-                        segment = part.get_payload(decode=True)
-                        if segment:
-			    fp.write(segment)
-			fp.close()
+    pass
 
 if __name__ == '__main__':
     main()
